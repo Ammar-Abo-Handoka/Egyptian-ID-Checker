3f69ef23eccdb6056763ac167bad969c298cf28f
Ammar Abo Handoka <ammar01032815787@gmail.com>
2026-07-08 17:19:43 +0300

    more reality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+print ("Please enter your details to apply for your personal ID card.")
 is_egyptian = input ("Are you Egyptian? type (Yes) or (No)\n").lower()
 if is_egyptian == "yes" :
     print ("Good, this is a first step,")
-    is_15 =input ("Are you above 15? Type (Yes) or (No)\n").lower()
-    if is_15 == "yes":
+    age = int(input ("How old are you?\n"))
+    if age > 15:
         birth = input ("Do you have a birth certificate? Type (Yes) or (No)\n").lower()
         if birth == "yes":
             print ("Congratulations! You can apply for a national ID card.")
@@ -10,10 +11,19 @@ if is_egyptian == "yes" :
             print ("You need a birth certificate before applying.")
         else :
             print ("Please answer with (Yes) or (NO)")
-    elif is_15 == "no" :
+    elif age == 15:
+        print ("This is your first ID card.")
+        birth = input ("Do you have a birth certificate? Type (Yes) or (No)\n").lower()
+        if birth == "yes":
+            print ("Congratulations! You can apply for a national ID card.")
+        elif birth == "no" :
+            print ("You need a birth certificate before applying.")
+        else :
+            print ("Please answer with (Yes) or (NO)")
+    elif age < 15 :
         print ("Sorry, You must be 15 years. \n You can try again when you are 15.")
     else :
-        print ("Please answer with (Yes) or (NO).")
+        print ("Please answer with your really age")
 elif is_egyptian =="no" :
     print("Sorry, The Egyptian ID is only for Egyptians.")
 else :
